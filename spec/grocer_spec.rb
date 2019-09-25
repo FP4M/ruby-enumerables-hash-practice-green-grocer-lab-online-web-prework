06d6a9b52b74fe21606c81b4fbade69d82ec5603
Prince Charles <princecharles35@yahoo.com>
2019-09-25 13:02:42 +0000

    Automatically backed up by Learn

---
+++ spec/grocer_spec.rb
@@ -0,0 +1,5 @@
+{
+  "PEANUT BUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
+}
